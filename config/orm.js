ca8c310131561887bd183b1e5cd3a237b89fdbbe
Mina G <mghaffar89@gmail.com>
2020-07-15 06:58:11 -0500

    set up rest of directory and made the orm methods

--- config/orm.js
+++ config/orm.js
@@ -3,13 +3,33 @@ const connection = require("./connection.js");
 const orm = {
   selectAll: function (table, cb) {
     const queryString = "SELECT * FROM ??;";
-    connection.query(queryString, function (error, result) {
-      if (error) throw error;
+    connection.query(queryString, [table], function (err, result) {
+      if (err) throw error;
       cb(result);
     });
   },
 
   insertOne: function (tableName, column, value, cb) {
-    const queryString = "INSERT INTO ";
+    const queryString = "INSERT INTO ?? (??) VALUES (?);";
+    connection.query(queryString, [table, columns, data], function (
+      err,
+      result
+    ) {
+      if (err) throw err;
+      cb(result);
+    });
+  },
+
+  updateOne: function (table, id, column, newvalue, cb) {
+    const queryString = "UPDATE ?? SET ?? = ? where id = ?;";
+    connection.query(queryString, [table, column, newvalue, id], function (
+      err,
+      result
+    ) {
+      if (err) throw err;
+      cb(result);
+    });
   },
 };
+
+module.exports = orm;

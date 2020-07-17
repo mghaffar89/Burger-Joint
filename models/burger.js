ddb37f0e912dc7408a26d650f7667b9a3f735f3b
Mina G <mghaffar89@gmail.com>
2020-07-16 23:27:53 -0500

    set up model file to use orm in functions, also made some css changes

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,24 @@
-var orm = require("../config/orm.js");
+const orm = require("../config/orm.js");
+
+const dbqueries = {
+  getBurgers: function (cb) {
+    orm.selectAll("burgers", function (results) {
+      burgers = results;
+      cb(results);
+    });
+  },
+  addBurger: function (burger_name, cb) {
+    orm.insertOne("burgers", ["burger_name"], [burger_name], function (
+      results
+    ) {
+      cb(results);
+    });
+  },
+  eatBurger: function (id, cb) {
+    orm.updateOne("burgers", id, "devoured", true, function (results) {
+      cb(results);
+    });
+  },
+};
+
+module.exports = dbqueries;

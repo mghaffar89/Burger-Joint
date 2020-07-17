f1ffa7ce509207d52a06b02b8b15c73b6009ccd3
Mina G <mghaffar89@gmail.com>
2020-07-17 04:19:19 -0500

    created router routes for get, post and update

--- controllers/burger_controllers.js
+++ controllers/burger_controllers.js
@@ -1,6 +1,33 @@
 const express = require("express");
 const router = express.Router();
 
-const burger = require("../models/burger.js");
+const dbqueries = require("../models/burger.js");
+
+router.get("/", function (request, result) {
+  dbqueries.getBurgers(function (data) {
+    burgers = data;
+    result.render("index", {
+      burgers: burgers,
+    });
+  });
+});
+
+router.post("/api/burgers", function (request, resultAPI) {
+  dbqueries.addBurger(request.body.new_burger, function (result) {
+    resultAPI.json({ id: result.insertId });
+  });
+});
+
+router.put("/api/burgers/:id", function (request, resultAPI) {
+  var burger_id = request.params.id;
+
+  dbqueries.eatBurger(burger_id, function (result) {
+    if (result.changedRows == 0) {
+      return resultAPI.status(404).end();
+    } else {
+      resultAPI.status(200).end();
+    }
+  });
+});
 
 module.exports = router;

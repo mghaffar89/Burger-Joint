5978a8e9b6e6e2283f5bd372078e699217e859bb
Mina G <mghaffar89@gmail.com>
2020-07-17 04:39:53 -0500

    added front end JS on click events that would move the burgers to the other list

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
-var express = require("express");
-var exphbs = require("express-handlebars");
-
-var PORT = process.env.PORT || 8080;
+const express = require("express");
+const exphbs = require("express-handlebars");
+const routes = require("./controllers/burger_controllers.js");
 
 var app = express();
 
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+var PORT = process.env.PORT || 8080;
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+var path = require("path");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+burgers = [];
+
+app.listen(PORT, function () {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
